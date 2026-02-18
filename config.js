03f2c748542640cff4d9119f7a25bd5e026ab105
eowert72jp <eowert72jp@gmail.com>
2026-02-18 12:07:54 +0000

    배포를 위해 config.js 포함

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase 클라이언트 설정을 위한 파일
+// 실제 프로젝트에서는 이 정보를 서버 환경 변수 등을 통해 안전하게 관리해야 합니다.
+
+const supabaseUrl = 'https://ygxyyqnonetpohvwrgnf.supabase.co'; // 이곳에 실제 Supabase 프로젝트 URL을 입력하세요.
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlneHl5cW5vbmV0cG9odndyZ25mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MDYyODgsImV4cCI6MjA4Njk4MjI4OH0.MRIWnTwHF2GWk1QGpjxFvUd_R_LCv2cfMnVY2UOtKvw'; // 이곳에 실제 Supabase anon 키를 입력하세요.
