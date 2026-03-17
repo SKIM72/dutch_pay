eb7038b1187a434a959635f39b229f710d9bd7e4
eowert72jp <eowert72jp@gmail.com>
2026-03-17 20:24:35 +0000

    구글 서치3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'settle-up-cache-v44'; // 코드가 변경될 때마다 버전 번호를 올려주세요!
+const CACHE_NAME = 'settle-up-cache-v46'; // 코드가 변경될 때마다 버전 번호를 올려주세요!
 const urlsToCache = [
   './',
   './index.html',
